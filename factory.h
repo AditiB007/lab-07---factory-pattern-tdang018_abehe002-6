4f7725e4de6a4f62afdeb929511baffcb0b25c50
tdang93 <aznnoodle101@hotmail.com>
2021-05-24 14:25:23 -0700

    Moved lab 3 files into /lab3 folder, began factory.h

---
+++ factory.h
@@ -0,0 +1,16 @@
+#ifndef __FACTORY_H__
+#define __FACTORY_H__
+
+#include "lab/base.hpp"
+
+class Factory {
+private:
+
+public:
+    Base* parse(char** input, int length) {
+      
+    }
+
+};
+
+#endif // __FACTORY_H__
